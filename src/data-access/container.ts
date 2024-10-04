7452f210e03be99218867b4d6e498dcdc8b52df9
AlefeVT <alefevt@gmail.com>
2024-10-04 17:30:56 -0300

    feat: initial container backend

---
+++ src/data-access/container.ts
@@ -0,0 +1,39 @@
+import { database } from "@/db";
+import { activityLogs, Container, ContainerId, containers } from "@/db/schema";
+import { ContainerData } from "@/interfaces/Container";
+import { eq } from "drizzle-orm";
+
+export async function getContainerById(containersId: ContainerId) {
+    return await database.query.containers.findFirst({
+      where: eq(containers.id, containersId),
+    });
+  }
+
+  export async function createContainer(container: ContainerData) {
+    await database.insert(containers).values(container);
+  }
+
+  export async function updateContainer(containerId: Container["id"], updatedData: Partial<Container>) {
+    await database.update(containers)
+      .set(updatedData)
+      .where(eq(containers.id, containerId));
+  }
+
+
+  export async function deleteContainer(containerId: Container["id"], userId: number) {
+    const currentDate = new Date();
+  
+    await database.update(containers)
+      .set({
+        deletedAt: currentDate, 
+      })
+      .where(eq(containers.id, containerId));
+
+      await database.insert(activityLogs).values({
+        userId,
+        action: 'container_deleted',
+        targetId: containerId,
+        targetType: 'container',
+        createdAt: currentDate,
+      });
+  }
